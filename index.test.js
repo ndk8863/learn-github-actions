bd7f0cd27ed6ed376b65729ac298ac7d9ad33b1b
ndk8863 <user1031@DESKTOP-KMVPL40.localdomain>
2025-12-07 15:57:41 +0900

    commit

--- index.test.js
+++ index.test.js
@@ -1,5 +1,5 @@
-const{sum} = require('./index')
+const { sum } = require('./index');
 
-TextDecoderStream('adds 1+2 to equal 3'),()=>{
+test('adds 1+2 to equal 3'),()=>{
     expect(sum(1,2).toBe(3));
 }
